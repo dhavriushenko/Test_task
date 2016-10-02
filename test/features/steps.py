c08a1a4d1572b4f6e21848483059f3e5b539fd6c
Den <dhavriushenko@gmail.com>
2016-10-02 20:30:55 +0200

    Update steps.py

--- test/features/steps.py
+++ test/features/steps.py
@@ -8,15 +8,44 @@ get_current_path = os.path.dirname(os.path.abspath(__file__))
 
 @step(u'I am using the application')
 def given_a_working_application(step):
-    print('Attempting to use application...')
+    print("Attempting to use application...")
     world.app = Output()
 
-@step(u'I input file with 7 lines "(\d+)"')
-def given_i_input_test_file(step):
-    test_file = get_current_path + '/test_input7_lines.txt'
-    world.result = world.app.outputlast5lines(test_file,5)
+@step(u'File with 5 lines')
+def given_test_file(step):
+    world.test_file = get_current_path + '/test_input7_lines.txt'
 
-@step(u'I see output of "([^"]+)"')
-def result(step, expected_result):
+@step(u'I use given file with following lines')
+def given_i_input_test_file(step):
+    world.result = world.app.outputlast5lines(world.test_file, 5)
+
+@step(u'I should see following output:')
+def result(step,expected_result):
+    actual_result = world.result
+    assert_equals(expected_result, actual_result)
+
+@step(u'File with 6 lines')
+def given_test_file(step):
+    world.test_file = get_current_path + '/test_input6_lines.txt'
+
+@step(u'I use given file with following lines')
+def given_i_input_test_file(step):
+    world.result = world.app.outputlast5lines(world.test_file,5)
+
+@step(u'I should see following output:')
+def result(step,expected_result):
+    actual_result = world.result
+    assert_equals(expected_result, actual_result)
+
+@step(u'File with 4 lines')
+def given_test_file(step):
+    world.test_file = get_current_path + '/test_input6_lines.txt'
+
+@step(u'I use given file with following lines')
+def given_i_input_test_file(step):
+    world.result = world.app.outputlast5lines(world.test_file,5)
+
+@step(u'I should see following output:')
+def result(step,expected_result):
     actual_result = world.result
     assert_equals(expected_result, actual_result)
