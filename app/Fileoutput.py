e7d45c802e5508f43b42b88e9770e60ea447a35d
Den <dhavriushenko@gmail.com>
2016-10-02 19:20:37 +0200

    Update Fileoutput.py

--- app/Fileoutput.py
+++ app/Fileoutput.py
@@ -1,21 +1,16 @@
-
 import sys
 import os
-
 
 if len(sys.argv) != 2:
     print('Usage: FileOutput.py <File> ')
     sys.exit(1)
-print(sys.argv[1])
+
 filename = sys.argv[1]
-#filename = '/Users/temp/PycharmProjects/Test_task/tests/features/test_input.txt'
 
 line = 5
 
-
 class Output(object):
-
-    def outputlast5lines(self, filename):
+    def outputlast5lines(self, filename, x):
         buffersize = 1024
         filesize = os.stat(filename).st_size
         iter = 0
@@ -28,8 +23,9 @@ class Output(object):
                 position = inf.tell()
                 inf.seek(filesize - buffersize * iter)
                 result.extend(inf.readlines())
-                if len(result) >= line or position == 0:
+                if len(result) >= x or position == 0:
                     print(''.join(result[-5::]))
                     break
 
-Output().outputlast5lines(filename)
+
+Output().outputlast5lines(filename, line)
