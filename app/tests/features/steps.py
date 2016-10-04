17e27c8b384f0fc8996230babcb80719fb3c690c
Den <dhavriushenko@gmail.com>
2016-10-04 23:05:51 +0200

    Update steps.py

--- app/tests/features/steps.py
+++ app/tests/features/steps.py
@@ -1,8 +1,12 @@
-import os ,sys
+# -*- coding: utf-8 -*-
+
+import sys, os
 from nose.tools import assert_equals
-from lettuce import step, world
+from lettuce import *
+#from tempfile import NamedTemporaryFile
+#only if ypu do not set PYTHONPATH
 sys.path.insert(1,'/Users/temp/PycharmProjects/Test_task/')
-from  app.FileOutput import Output
+from app.FileOutput import Output
 
 get_current_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -11,41 +15,47 @@ def given_a_working_application(step):
     print("Attempting to use application...")
     world.app = Output()
 
-@step(u'File with 5 lines')
-def given_test_file(step):
-    world.test_file = get_current_path + '/test_input7_lines.txt'
 
-@step(u'I use given file with following lines')
+@step(u'File with following data types \'(.*)\'')
+def given_test_file(step, input):
+    with open(world.path,'w') as inf:
+        inf.write(bytes(input))
+
+
+@step(u'I use given file')
 def given_i_input_test_file(step):
-    world.result = world.app.outputlast5lines(world.test_file, 5)
+    world.app = Output()
+    world.result = world.app.outputlast5lines(world.path, 5)
 
-@step(u'I should see following output:')
-def result(step,expected_result):
+
+@step(u'I should see following \'(.*)\'')
+def result(step, expected_result):
     actual_result = world.result
     assert_equals(expected_result, actual_result)
 
-@step(u'File with 6 lines')
-def given_test_file(step):
-    world.test_file = get_current_path + '/test_input6_lines.txt'
 
-@step(u'I use given file with following lines')
+@step(u'File with \'(.*)\' lines \'(.*)\'')
+def given_test_file(step, line, input):
+    with open(world.path,'w') as inf:
+        i=1
+        j=int(line)
+        while j >= i:
+            if j == i:
+                inf.writelines([input])
+            else:
+                inf.write(input)
+                inf.write('\n')
+            i+=1
+
+@step(u'I use given file')
 def given_i_input_test_file(step):
-    world.result = world.app.outputlast5lines(world.test_file,5)
+    world.app = Output()
+    world.result = world.app.outputlast5lines(world.path, 5)
 
-@step(u'I should see following output:')
-def result(step,expected_result):
-    actual_result = world.result
-    assert_equals(expected_result, actual_result)
 
-@step(u'File with 4 lines')
-def given_test_file(step):
-    world.test_file = get_current_path + '/test_input6_lines.txt'
+@step(u'I should see (.*) lines')
+def result(step, expected_result):
+    result = world.result
+    actual_result = len(result.split('\n'))
+    assert_equals(int(expected_result), actual_result)
 
-@step(u'I use given file with following lines')
-def given_i_input_test_file(step):
-    world.result = world.app.outputlast5lines(world.test_file,5)
-
-@step(u'I should see following output:')
-def result(step,expected_result):
-    actual_result = world.result
-    assert_equals(expected_result, actual_result)
